e719e002b0dc7269c33818fe48aced5d48588e1e
Merenda Franco N <merenda.franco83@gmail.com>
2025-02-19 11:28:35 +0100

    Added dotenv and configs to handle api keys and custom configuratins

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "SECRET_KEY"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') \
-                              or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    OPEN_WEATHER_API_KEY = os.environ.get("OPEN_WEATHER_API_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "SECRET_KEY"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URI"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = "simple"
+    CACHE_DEFAULT_TIMEOUT = 300
