761ee32c6a445ff484af5e120d4b8b506bbbd68e
Merenda Franco N <merenda.franco83@gmail.com>
2025-02-19 18:25:18 +0100

    Refactored `Users` to `User`

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,7 +2,7 @@ import flask_login
 from flask import Blueprint, render_template, request, redirect, url_for
 
 from ..extensions import db, bcrypt
-from ..models import Users
+from ..models import User
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -14,7 +14,7 @@ def register():
         # Generate a salt and hash the password
 
         hashed_password = bcrypt.generate_password_hash(bytes)
-        user = Users(username=request.form.get("username"), password=hashed_password)
+        user = User(username=request.form.get("username"), password=hashed_password)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for("auth.login"))
@@ -24,10 +24,12 @@ def register():
 @auth_blueprint.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        db_user = Users.query.filter_by(username=request.form.get("username")).first()
+        db_user = User.query.filter_by(username=request.form.get("username")).first()
         pwd_form_bytes = request.form.get("password").encode()
 
-        if db_user and bcrypt.check_password_hash(db_user.password, pwd_form_bytes):
+        if db_user and bcrypt.check_password_hash(
+            db_user.password_hash, pwd_form_bytes
+        ):
             flask_login.login_user(db_user)
             return redirect(url_for("home.home"))
     return render_template("login.html")
--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from ..extensions import db
 
 
-class Users(UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
-    password = db.Column(db.String(250), nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
